de128d24e21c659575bb69b877d6f5c8ba85beac
yared <yaredyaya16@gmail.com>
2020-01-12 00:47:02 +0300

    updating General_gorm.go

--- hospital_server/Doctor/repository/General_gorm.go
+++ hospital_server/Doctor/repository/General_gorm.go
@@ -1,9 +1,10 @@
 package repository
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
-	"github.com/web1_group_project/hospital_server/Doctor"
-	"github.com/web1_group_project/hospital_server/entity"
+	"github.com/monday271/hospital_server/Doctor"
+	"github.com/monday271/hospital_server/entity"
 	//"github.com/yaredsolomon/webProgram1/hospital/request"
 )
 
@@ -33,4 +34,26 @@ func (generalRepo *GeneralGormRepo) Laboratorists() ([]entity.Laboratorist, []er
 		return nil, errs
 	}
 	return laboratorists, errs
+}
+func (generalRepo *GeneralGormRepo) Users(id int, password string) (*entity.Profile, []error) {
+
+	fmt.Println(" thise is the gorm method")
+	fmt.Print(id)
+	users := entity.Profile{}
+	errs := generalRepo.conn.Where("id =?", id).First(&users).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	fmt.Println("user input")
+	fmt.Println(password)
+	fmt.Println("fetched data")
+	fmt.Println(users.Password)
+
+	if users.Password == password {
+		fmt.Println("thise is the gorm result")
+		fmt.Println(users)
+		return &users, errs
+	}
+	return nil, nil
+
 }
