af0f5161cfb4d01d5697b5336a2f4aea066ff0d4
yared <yaredyaya16@gmail.com>
2020-01-12 00:47:54 +0300

    updating General_service.go

--- hospital_server/Doctor/service/General_service.go
+++ hospital_server/Doctor/service/General_service.go
@@ -3,10 +3,9 @@ package service
 import (
 	//"github.com/yaredsolomon/webProgram1/hospital/entity"
 	//"github.com/yaredsolomon/webProgram1/hospital/request"
-
+	"github.com/monday271/hospital_server/Doctor"
+	"github.com/monday271/hospital_server/entity"
 	//"github.com/yaredsolomon/webProgram1/hospital/request"
-	"github.com/web1_group_project/hospital_server/Doctor"
-	"github.com/web1_group_project/hospital_server/entity"
 )
 
 // AppointmentService implements request.AppointmentService interface
@@ -29,6 +28,13 @@ func (gs *GeneralService) Pharmacists() ([]entity.Pharmacist, []error) {
 }
 func (gs *GeneralService) Laboratorists() ([]entity.Laboratorist, []error) {
 	generals, errs := gs.generalRepo.Laboratorists()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return generals, errs
+}
+func (gs *GeneralService) Users(id int, password string) (*entity.Profile, []error) {
+	generals, errs := gs.generalRepo.Users(id, password)
 	if len(errs) > 0 {
 		return nil, errs
 	}
