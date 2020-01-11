7e81e6c4098d3f4989b7365eb770a415a8f39c78
yared <yaredyaya16@gmail.com>
2020-01-12 00:45:31 +0300

    changing General_handler.go

--- hospital_server/delivery/http/handler/General_handler.go
+++ hospital_server/delivery/http/handler/General_handler.go
@@ -4,9 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	_ "fmt"
-	"github.com/julienschmidt/httprouter"
-	"github.com/web1_group_project/hospital_server/Doctor"
+	"github.com/monday271/hospital_server/Doctor"
+	"github.com/monday271/hospital_server/entity"
+	//"github.com/yaredsolomon/webProgram1/hospital/request"
 	"net/http"
+	//"github.com/betsegawlemma/restaurant-rest/comment"
+	"github.com/julienschmidt/httprouter"
+	_ "github.com/yaredsolomon/webProgram1/hospital/entity"
+	//"github.com/yaredsolomon/webProgram1/hospital/request"
 )
 
 //"github.com/yaredsolomon/webProgram1/sathurday18/entity"
@@ -69,4 +74,63 @@ func (gh *GeneralHandler) GetLaboratorists(w http.ResponseWriter,
 	w.Write(output)
 	return
 
+}
+func (dah *GeneralHandler) GetUsers(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Println(" I am at the put method")
+	//id, err := strconv.Atoi(ps.ByName("id"))
+	//fmt.Println(id)
+	//if err != nil {
+	//	w.Header().Set("Content-Type", "application/json")
+	//	http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	//	return
+	//}
+
+	//appointment, errs := dah.appointmentService.Appointment(uint(id))
+	users := &entity.Profile{}
+	//if len(errs) > 0 {
+	//	w.Header().Set("Content-Type", "application/json")
+	//	http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	//	return
+	//}
+	fmt.Println(" i have get single value")
+
+	l := r.ContentLength
+
+	body := make([]byte, l)
+
+	r.Body.Read(body)
+
+	json.Unmarshal(body, &users)
+
+	id := users.ID
+	password := users.Password
+	users, errs := dah.generalService.Users(int(id), password)
+	fmt.Println("aaaaaaaaaaaaa")
+	fmt.Println(users)
+	if len(errs) > 0 {
+		fmt.Println("thise is status not found")
+
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	if users == nil {
+		fmt.Println("thise is status not found")
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	output, err := json.MarshalIndent(users, "", "\t\t")
+
+	if err != nil {
+		fmt.Println("thise is status not found")
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
 }
